021140e993fe6799c7e3a00b52d1426751d4d1c0
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-05-08 19:24:42 +0530

    Add files via upload
    This program is for multithreading concept.
    Two Threads will be printed on some conditions :-
    1.Thread 0 will be printed in addition with even numbers from 1 to 10
    2.Thread 1 will be printed in addition with odd numbers from 1 to 10

---
+++ TestThread1.java
@@ -0,0 +1,26 @@
+class TestThread1 extends Thread{
+    public static void main(String... arr){
+        TestThread1 t1=new TestThread1();
+        TestThread1 t2=new TestThread1();
+        // t1.setName("KIT");
+        // t2.setName("KIP");
+        t1.start();
+        t2.start();
+        
+    }
+    public void run(){
+        if(Thread.currentThread().getName().equals("Thread-0")){      
+            for(int i=1;i<=10;i++){
+                if(i%2==0)
+                System.out.println(i+" "+Thread.currentThread().getName());
+            }
+        }
+        else {
+            for(int i=0;i<10;i++){
+                if(i%2!=0)
+                System.out.println(i+" "+Thread.currentThread().getName());
+            }
+        }
+    }
+
+}
