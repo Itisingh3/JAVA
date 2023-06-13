597e84c9c75f1c460aba065323316adfc7ebf1d8
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-06-13 23:39:48 +0530

    Update Score.java

--- QuizApplication/src/Score.java
+++ QuizApplication/src/Score.java
@@ -15,7 +15,7 @@ public class Score extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        ImageIcon i1 = new ImageIcon("C:\\Users\\Iti Singh\\Documents\\java developer\\Images\\score.png");
+        ImageIcon i1 = new ImageIcon("path for score image");
         Image i2 = i1.getImage().getScaledInstance(300, 250, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
